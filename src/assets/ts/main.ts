52110524c410d2820bc8d4d0613d1eccf29767b8
yonemoto <yonemoto@tam-tam.co.jp>
2020-04-02 02:35:28 +0900

    Add: mixin sass

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -3,19 +3,19 @@ import "./polyfill";
 
 // Namespace（プロジェクト開始時には以下削除してください）
 import commonMenu from "./namespace/common/commonMenu";
-import commonAccordion from "./namespace/common/commonAccordion";
 import commonSmoothScroll from "./namespace/common/commonSmoothScroll";
-import commonTab from "./namespace/common/commonTab";
+import topTab from "./namespace/top/topTab";
+import topAccordion from "./namespace/top/topAccordion";
 import topTextReturn from "./namespace/top/topTextReturn";
 import sampleJsonGet from "./namespace/sample/sampleJsonGet";
 import sample02Count from "./namespace/sample02/sample02Count";
 
 // Common
 commonMenu();
-commonAccordion();
 commonSmoothScroll();
-commonTab();
 // Top
+topAccordion();
+topTab();
 topTextReturn();
 // Sample
 sampleJsonGet();
--- src/styleguide-dev/components/interactive/tab/README.md
+++ src/styleguide-dev/components/interactive/tab/README.md
@@ -1,1 +1,6 @@
 ## タブナビゲーションモジュールに関するメモ
+WAI-ARIA対応のタブナビゲーションです。
+
+[Example of Tabs with Automatic Activation](https://www.w3.org/TR/wai-aria-practices/examples/tabs/tabs-1/tabs.html)
+
+[Accessible Rich Internet Applications (WAI-ARIA) 1.1 日本語訳](https://momdo.github.io/wai-aria-1.1/)
